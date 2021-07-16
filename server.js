39c11a9a6fc90c55217330e5a668096227e8af6e
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-07-16 10:41:20 -0400

    updated server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import logger from 'morgan'
 import methodOverride from 'method-override'
 
 import { router as indexRouter } from './routes/index.js'
-import { router as todosRouter } from './routes/todos.js'
+import { router as skillsRouter } from './routes/skills.js'
 
 const app = express()
 
@@ -37,7 +37,7 @@ express.static(
 
 app.use(methodOverride('_method'))
 app.use('/', indexRouter)
-app.use('/todos', todosRouter)
+app.use('/skills', skillsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -48,7 +48,7 @@ app.use(function (req, res, next) {
     app.use(function (err, req, res, next) {
       // set locals, only providing error in development
       res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+      res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
   res.status(err.status || 500)
