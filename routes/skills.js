9394225949cbd789b15e92e2c138786a8d500814
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-07-16 09:20:10 -0400

    updated routes

--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 const router = Router()
+import * as skillsCtrl from '../controllers/skills.js'
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource')
-})
+router.get('/', skillsCtrl)
+router.get('/:id', skillsCtrl.show)
+router.get('/new', skillsCtrl.new)
+router.get('/.post', skillsCtrl.create)
+router.get('/delete', skillsCtrl.delete)
+
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource')
+// })
 
 export {
   router
